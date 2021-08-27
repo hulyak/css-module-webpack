405e79ba3fbbaa32d528984fe8603a796d3ab60d
hulyak <hulyakarakayaa06@gmail.com>
2021-08-26 21:00:42 -0700

    remove mini-css-extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   entry: {
     main: "./src/index.js",
